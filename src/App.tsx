4ba9e29acc849341419bcb2944c8639601d0f733
Mark Sedrak <mark.sedrak@digio.com.au>
2023-12-14 17:51:28 -0300

    feat: invoke api from ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { Button, Container, LoadingOverlay, MantineProvider, Stack, Text } from "@mantine/core";
 import { theme } from "./theme";
+import { useState } from "react";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  
+  const [loading, setLoading] = useState(false);
+  const [counter, setCounter] = useState('-');
+
+  const incrementCounter = async () => {
+    setLoading(true)
+    const response = await (await fetch(`/api/counter`)).json()
+    setCounter(response.counterValue);
+    setLoading(false)
+  }
+
+
+  return <MantineProvider theme={theme}>
+
+    <Container>
+      <LoadingOverlay visible={loading} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />
+      <Stack p={"lg"} gap={'xl'}>
+        <Text>Bam! 😄</Text>
+        <Text>Count: {counter}</Text>
+        <Button onClick={incrementCounter}>Increment Counter</Button>
+      </Stack>
+    </Container>
+
+  </MantineProvider>;
 }
